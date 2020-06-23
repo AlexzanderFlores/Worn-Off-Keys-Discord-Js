a7975b356e948adb738ce708d2d060ebf48362f9
unknown <alexzanderflores@nestnetworking.com>
2020-06-23 12:00:44 -0700

    Add video 4

---
+++ 4-List-Servers-Delete-Messages-Bot-Status/index.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+const config = require('./config.json')
+const command = require('./command')
+
+client.on('ready', () => {
+  console.log('The client is ready!')
+
+  command(client, ['ping', 'test'], (message) => {
+    message.channel.send('Pong!')
+  })
+
+  command(client, 'servers', (message) => {
+    client.guilds.cache.forEach((guild) => {
+      message.channel.send(
+        `${guild.name} has a total of ${guild.memberCount} members`
+      )
+    })
+  })
+
+  command(client, ['cc', 'clearchannel'], (message) => {
+    if (message.member.hasPermission('ADMINISTRATOR')) {
+      message.channel.messages.fetch().then((results) => {
+        message.channel.bulkDelete(results)
+      })
+    }
+  })
+
+  command(client, 'status', (message) => {
+    const content = message.content.replace('!status ', '')
+    // "!status hello world" -> "hello world"
+
+    client.user.setPresence({
+      activity: {
+        name: content,
+        type: 0,
+      },
+    })
+  })
+})
+
+client.login(config.token)
