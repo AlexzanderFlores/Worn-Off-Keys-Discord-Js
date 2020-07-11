832ecf467ad76e0909c8b07329a0bfae9e26ad58
Alexzander Flores <alexzanderflores@nestnetworking.com>
2020-07-11 01:27:06 -0700

    Add video 12: Ban and kick command

---
+++ 12-Ban-and-Kick/command.js
@@ -0,0 +1,20 @@
+const { prefix } = require('./config.json')
+
+module.exports = (client, aliases, callback) => {
+  if (typeof aliases === 'string') {
+    aliases = [aliases]
+  }
+
+  client.on('message', (message) => {
+    const { content } = message
+
+    aliases.forEach((alias) => {
+      const command = `${prefix}${alias}`
+
+      if (content.startsWith(`${command} `) || content === command) {
+        console.log(`Running the command ${command}`)
+        callback(message)
+      }
+    })
+  })
+}
---
+++ 12-Ban-and-Kick/index.js
@@ -0,0 +1,59 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+const config = require('./config.json')
+const command = require('./command')
+
+client.on('ready', () => {
+  console.log('The client is ready!')
+
+  command(client, 'ban', (message) => {
+    const { member, mentions } = message
+
+    const tag = `<@${member.id}>`
+
+    if (
+      member.hasPermission('ADMINISTRATOR') ||
+      member.hasPermission('BAN_MEMBERS')
+    ) {
+      const target = mentions.users.first()
+      if (target) {
+        const targetMember = message.guild.members.cache.get(target.id)
+        targetMember.ban()
+        message.channel.send(`${tag} That user has been`)
+      } else {
+        message.channel.send(`${tag} Please specify someone to ban.`)
+      }
+    } else {
+      message.channel.send(
+        `${tag} You do not have permission to use this command.`
+      )
+    }
+  })
+
+  command(client, 'kick', (message) => {
+    const { member, mentions } = message
+
+    const tag = `<@${member.id}>`
+
+    if (
+      member.hasPermission('ADMINISTRATOR') ||
+      member.hasPermission('KICK_MEMBERS')
+    ) {
+      const target = mentions.users.first()
+      if (target) {
+        const targetMember = message.guild.members.cache.get(target.id)
+        targetMember.kick()
+        message.channel.send(`${tag} That user has kicked`)
+      } else {
+        message.channel.send(`${tag} Please specify someone to kick.`)
+      }
+    } else {
+      message.channel.send(
+        `${tag} You do not have permission to use this command.`
+      )
+    }
+  })
+})
+
+client.login(config.token)
