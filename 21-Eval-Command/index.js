7e7e54b38cb7e2684e92995e68901fac1d49adb3
Alexzander Flores <alexzanderflores@nestnetworking.com>
2020-07-18 03:01:47 -0700

    Add video 21: Eval command

---
+++ 21-Eval-Command/index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+const config = require('./config.json')
+
+client.on('ready', async () => {
+  console.log('The client is ready!')
+})
+
+client.login(config.token)
