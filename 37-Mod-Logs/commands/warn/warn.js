765ccf9cbdcc28e3d4cd2075d826c91efdd415c4
Alexzander Flores <alexzanderflores@nestnetworking.com>
2020-07-31 09:30:53 -0700

    Add video 37: Mod logs

---
+++ 37-Mod-Logs/commands/warn/warn.js
@@ -0,0 +1,51 @@
+const mongo = require('../../mongo')
+const warnSchema = require('../../schemas/warn-schema')
+
+module.exports = {
+  commands: 'warn',
+  minArgs: 2,
+  expectedArgs: "<Target user's @> <reason>",
+  requiredRoles: ['Moderator'],
+  callback: async (message, arguments) => {
+    const target = message.mentions.users.first()
+    if (!target) {
+      message.reply('Please specify someone to warn.')
+      return
+    }
+
+    arguments.shift()
+
+    const guildId = message.guild.id
+    const userId = message.member.id
+    const reason = arguments.join(' ')
+
+    const warning = {
+      author: message.member.user.tag,
+      timestamp: new Date().getTime(),
+      reason,
+    }
+
+    await mongo().then(async (mongoose) => {
+      try {
+        await warnSchema.findOneAndUpdate(
+          {
+            guildId,
+            userId,
+          },
+          {
+            guildId,
+            userId,
+            $push: {
+              warnings: warning,
+            },
+          },
+          {
+            upsert: true,
+          }
+        )
+      } finally {
+        mongoose.connection.close()
+      }
+    })
+  },
+}
---
+++ 37-Mod-Logs/mod-logs.js
@@ -0,0 +1,25 @@
+const roles = ['Moderator']
+
+module.exports = (client) => {
+  const channelId = '738788649113747568'
+
+  client.on('message', (message) => {
+    const { guild, content, member } = message
+
+    if (member.user.bot) {
+      console.log('Ignoring bot')
+      return
+    }
+
+    const hasRole = member.roles.cache.find((role) => {
+      return roles.includes(role.name)
+    })
+
+    if (hasRole) {
+      const channel = guild.channels.cache.get(channelId)
+      channel.send(`<@${member.id}> said this:
+      
+"${content}"`)
+    }
+  })
+}
