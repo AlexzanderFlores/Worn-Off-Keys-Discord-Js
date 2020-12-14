28cf4bb187345b04cbb78e8c54a560e20c5dabfc
rinarchy <51412696+rinarchy@users.noreply.github.com>
2020-12-14 21:54:52 +0000

    Update help.js
    In the module.exports: 
    
    permissions = [],
    
    is defined and by default the permission check uses let permissions = command.permission which is not used in the add or add-economy command. This leads to all commands being shown for all users irrespective of their actual permissions. Changed permissions to commandPermissions to be prevent overlap with existing variable and to be less ambiguous & to function as intended.

--- 29-Dynamic-Help-Menu/commands/help.js
+++ 29-Dynamic-Help-Menu/commands/help.js
@@ -2,46 +2,46 @@ const loadCommands = require('./load-commands')
 const { prefix } = require('../config.json')
 
 module.exports = {
-  commands: ['help', 'h'],
-  description: "Describes all of this bot's commands",
-  callback: (message, arguments, text) => {
-    let reply = 'I am the tutorial bot, here are my supported commands:\n\n'
+    commands: ['help', 'h'],
+    description: "Describes all of this bot's commands",
+    callback: (message, arguments, text) => {
+        let reply = 'Here are my supported commands:\n\n'
 
-    const commands = loadCommands()
+        const commands = loadCommands()
 
-    for (const command of commands) {
-      // Check for permissions
-      let permissions = command.permission
+        for (const command of commands) {
+            // Check for permissions
+            let commandPermissions = command.permissions
 
-      if (permissions) {
-        let hasPermission = true
-        if (typeof permissions === 'string') {
-          permissions = [permissions]
+            if (commandPermissions) {
+                let hasPermission = true
+                if (typeof commandPermissions === 'string') {
+                    commandPermissions = [commandPermissions]
+                }
+
+                for (const permission of commandPermissions) {
+                    if (!message.member.hasPermission(permission)) {
+                        hasPermission = false
+                        break
+                    }
+                }
+
+                if (!hasPermission) {
+                    continue
+                }
+            }
+
+            // Format the text
+            const mainCommand =
+                typeof command.commands === 'string'
+                    ? command.commands
+                    : command.commands[0]
+            const args = command.expectedArgs ? ` ${command.expectedArgs}` : ''
+            const { description } = command
+
+            reply += `**${prefix}${mainCommand}${args}** = ${description}\n`
         }
 
-        for (const permission of permissions) {
-          if (!message.member.hasPermission(permission)) {
-            hasPermission = false
-            break
-          }
-        }
-
-        if (!hasPermission) {
-          continue
-        }
-      }
-
-      // Format the text
-      const mainCommand =
-        typeof command.commands === 'string'
-          ? command.commands
-          : command.commands[0]
-      const args = command.expectedArgs ? ` ${command.expectedArgs}` : ''
-      const { description } = command
-
-      reply += `**${prefix}${mainCommand}${args}** = ${description}\n`
-    }
-
-    message.channel.send(reply)
-  },
+        message.channel.send(reply)
+    },
 }
